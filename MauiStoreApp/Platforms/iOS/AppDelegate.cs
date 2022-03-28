2665df32f4e75b3085e69ea3d5fd50d098bec2bb
bartvanhoey <bartvanhoey@hotmail.com>
2022-03-28 11:43:00 +0200

    Initial commit

---
+++ MauiStoreApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiStoreApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
